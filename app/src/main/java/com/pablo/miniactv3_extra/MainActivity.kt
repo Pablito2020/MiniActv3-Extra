b0ce29493e8111d73cda9ad921f752219bd4b958
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2022-03-20 19:37:45 +0100

    Add intent-action for sharing text

--- app/src/main/java/com/pablo/miniactv3_extra/MainActivity.kt
+++ app/src/main/java/com/pablo/miniactv3_extra/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.pablo.miniactv3_extra
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.pablo.miniactv3_extra.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.resultText.text = intent.getStringExtra(Intent.EXTRA_TEXT)
     }
 }
